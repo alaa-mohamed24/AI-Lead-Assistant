7da06eb47a13701cf2932433594fea6df15621dd
alaa-mohamed24 <lily.m.soby24@gmail.com>
2026-08-27 22:03:15 +0300

    Add exception handling to ask_gemini

--- src/ai_lead_assistant/test_main.py
+++ src/ai_lead_assistant/test_main.py
@@ -1,39 +1,51 @@
-from src.ai_lead_assistant.main import handel_message
-
-conversation_history =[]
-
-
-messages=  [
-    "I want an apartment in Sheikh Zayed.",
-    "My budget is 4 million.",
-    
-]
-for message in messages: 
-    ai_response, lead, analysis, lead_id = handel_message(
-            conversation_history,
-            message
-        )
-
-    print("\nUser:")
-    print(message)
-
-    print("\nAI:")
-    print(ai_response)
-
-    print("\nLead:")
-    print(lead)
-
-    print("\nAnalysis:")
-    print(analysis)
-
-# import os
-# from dotenv import load_dotenv
-
-# # تحميل المتغيرات من ملف .env
-# load_dotenv()
-
-# # قراءة الـ API Key من البيئة
-# api_key = os.getenv("GOOGLE_API_KEY") 
+from src.ai_lead_assistant.models import Lead, PropertyType, FinishingType
+from src.ai_lead_assistant.database import create_table, save_lead, update_lead, get_lead_by_id
 
 
-# print("API KEY:", repr(api_key))
+# 1. Create table
+create_table()
+
+
+# 2. Create first lead
+lead = Lead(
+    name="Ahmed",
+    phone="01012345678",
+    property_type=PropertyType.APARTMENT,
+    location="Sheikh Zayed",
+    budget=4000000,
+    bedrooms=3,
+    finishing=FinishingType.FINISHED,
+    timeline="immediate",
+    intent="buy"
+)
+
+
+# 3. Save lead for the first time
+lead_id = save_lead(
+    lead=lead,
+    score=75,
+    classification="warm"
+)
+
+print("Created Lead ID:", lead_id)
+
+
+# 4. Update the same lead
+lead.budget = 6000000
+lead.bedrooms = 4
+
+updated_id = update_lead(
+    lead_id=lead_id,
+    lead=lead,
+    score=90,
+    classification="hot"
+)
+
+print("Updated Lead ID:", updated_id)
+
+
+# 5. Read lead from database
+saved_lead = get_lead_by_id(updated_id)
+
+print("\n--- SAVED LEAD ---")
+print(saved_lead)
